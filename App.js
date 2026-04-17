fe26045fc61050f1f0fb476b19e7cd09552630e5
Samuel do Nascimento de Alencar <samuel_nascimento@hotmail.com>
2026-04-17 16:03:29 -0300

    chore: update App.js greeting and refine package.json scripts for Android and iOS builds

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello CAOA!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
